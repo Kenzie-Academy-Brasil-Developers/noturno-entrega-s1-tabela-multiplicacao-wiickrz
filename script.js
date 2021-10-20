c94af65e1e0ddbfe56bee3d0ad9663dd250504fe
wiickrz <will.cs.182@gmail.com>
2021-10-20 20:49:41 -0300

    Solved

---
+++ script.js
@@ -0,0 +1,20 @@
+        /* 
+            Entrega Aula 45 - William Heckler
+                                              */
+
+let multTable = (size) => {
+    let firstArray = []
+    let secondArray = []
+
+    for (let firstCounter = 0; firstCounter <= size; firstCounter++) {
+        firstArray[firstCounter] = []
+
+        for (let secondCounter = 0; secondCounter <= size; secondCounter++) {
+            secondArray.push(secondCounter)
+
+            firstArray[firstCounter].push(firstCounter * secondCounter)
+        }
+    }
+    return firstArray
+}
+console.table(multTable(10))
